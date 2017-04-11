778f633b134a39d87036eef9adf548db692361b5
Mina Slater <slater.mina@gmail.com>
2017-04-10 23:41:42 -0500

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411030552) do
+ActiveRecord::Schema.define(version: 20170411043643) do
 
   create_table "interpreters", force: :cascade do |t|
     t.string   "first_name",                        null: false
@@ -24,6 +24,36 @@ ActiveRecord::Schema.define(version: 20170411030552) do
     t.index ["last_name"], name: "index_interpreters_on_last_name"
     t.index ["performance_avail"], name: "index_interpreters_on_performance_avail"
     t.index ["rehearsal_avail"], name: "index_interpreters_on_rehearsal_avail"
+  end
+
+  create_table "lighting_vendors", force: :cascade do |t|
+    t.string   "name",                       null: false
+    t.boolean  "deliver",    default: false
+    t.boolean  "rental",     default: false
+    t.boolean  "sales",      default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["deliver"], name: "index_lighting_vendors_on_deliver"
+    t.index ["rental"], name: "index_lighting_vendors_on_rental"
+    t.index ["sales"], name: "index_lighting_vendors_on_sales"
+  end
+
+  create_table "rehearsal_spaces", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "cost"
+    t.string   "cost_unit"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cost"], name: "index_rehearsal_spaces_on_cost"
+    t.index ["cost_unit"], name: "index_rehearsal_spaces_on_cost_unit"
+  end
+
+  create_table "scene_shops", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
