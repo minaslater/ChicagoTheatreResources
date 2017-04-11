abf7fbb00af780becdfef26f3f2b72290d8bdb13
Mina Slater <slater.mina@gmail.com>
2017-04-10 22:34:19 -0500

    migration for interpreters

--- db/migrate/20170411030552_create_interpreters.rb
+++ db/migrate/20170411030552_create_interpreters.rb
@@ -1,12 +1,14 @@
 class CreateInterpreters < ActiveRecord::Migration[5.0]
   def change
     create_table :interpreters do |t|
-      t.string :name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :accreditation, null: false
       t.boolean :rehearsal_avail, default: false
       t.boolean :performance_avail, default: false
 
       t.timestamps
     end
+    add_index :first_name, :last_name, :rehearsal_avail, :performance_avail
   end
 end
