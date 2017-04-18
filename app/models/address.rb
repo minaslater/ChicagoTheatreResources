762199af7dadbf6055768e3ad08d958b5f2439dd
Mina Slater <slater.mina@gmail.com>
2017-04-18 14:00:40 -0500

    pulls out address method from controller  to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :location, polymorphic: true
+
+  def combine
+    address = street1
+    address += "\n#{street2}" if street2
+    address += "\n#{city}, #{state} #{zip_code}"
+    address
+  end
 end
