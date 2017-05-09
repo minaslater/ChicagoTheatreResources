be280ffb18ac1d575815e5504441bcb87fb000a9
Mina Slater <slater.mina@gmail.com>
2017-05-09 10:19:20 -0500

    adds validations for update action

--- app/controllers/interpreters_controller.rb
+++ app/controllers/interpreters_controller.rb
@@ -38,10 +38,16 @@ class InterpretersController < ApplicationController
 
   def update
     interpreter_to_update = Interpreter.find(params[:id])
-    interpreter_to_update.update(interpreter_params)
-    interpreter_to_update.emails.update(email_params)
-    interpreter_to_update.phones.update(phone_params)
-    redirect_to action: "index"
+    interpreter_to_update.assign_attributes(interpreter_params)
+    if interpreter_to_update.save
+      update_email(interpreter_to_update)
+      update_phone(interpreter_to_update)
+      flash[:notice] = "Update successful!"
+      redirect_to action: "index"
+    else
+      flash[:alert] = "Oops, something went wrong."
+      redirect_to :back
+    end
   end
 
   def destroy
@@ -60,5 +66,23 @@ class InterpretersController < ApplicationController
 
   def phone_params
     params.require(:interpreter).permit(phone: :number)["phone"]
+  end
+
+  def update_email(interpreter)
+    if interpreter.emails
+      interpreter.emails.update(email_params)
+    else
+      new_email = new_interpreter.emails.new(email_params)
+      new_email.save if new_email.valid?
+    end
+  end
+
+  def update_phone(interpreter)
+    if interpreter.phones
+      interpreter.phones.update(phone_params)
+    else
+      new_phone = new_interpreter.phones.new(phone_params)
+      new_phone.save if new_email.valid?
+    end
   end
 end
